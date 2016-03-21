28cbab713e2630d6168d954b9f6ab9a8253ded14
SeraZheng <462970213@qq.com>
2016-03-21 14:49:39 +0800

    Initialize project
    Initialize project

---
+++ GitHouse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHouse
+//
+//  Created by 郑少博 on 16/3/21.
+//  Copyright © 2016年 郑少博. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# GitHouse
+Git client for GitHub with Swift 2.0
